128bfee6d3a4368da7471c522f8d6d5748b569de
olgagaleta <olga.galeta.og@gmail.com>
2022-12-19 15:10:51 +0000

    add changes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,14 +7,22 @@ import ContactsForm from './ContactsForm/ContactsForm';
 
 class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
+
+  componentDidMount() {
+    const contactsSaved = JSON.parse(localStorage.getItem('contacts'));
+    if (contactsSaved) {
+      this.setState({ contacts: contactsSaved });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
 
   addContact = number => {
     const searchRepeat = this.state.contacts
